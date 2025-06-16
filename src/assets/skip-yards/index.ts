ddce5774797a4f7966247ea693891016b3ee62bf
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-06-16 22:21:44 +0300

    feat: Add yard skip images

---
+++ src/assets/skip-yards/index.ts
@@ -0,0 +1,7 @@
+export { default as Yard4 } from "./yard-4.png";
+export { default as Yard6 } from "./yard-6.png";
+export { default as Yard8 } from "./yard-8.png";
+export { default as Yard10 } from "./yard-10.png";
+export { default as Yard12 } from "./yard-12.png";
+export { default as Yard14 } from "./yard-14.png";
+export { default as Yard40 } from "./yard-40.png";
